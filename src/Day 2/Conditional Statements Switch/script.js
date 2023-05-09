1988e825e821e8fe7a6f5dd94389ec600cf881d8
geekyharsh05 <hp424805@gmail.com>
2023-05-09 16:12:31 +0530

    :sparkles: feat: add Day 2 Problems

---
+++ src/Day 2/Conditional Statements Switch/script.js
@@ -0,0 +1,25 @@
+/**
+ * @author geekyharsh05
+ */
+
+const getLetter = (s) => {
+  let letter;
+  // Write your code here
+  switch (s.charAt(0)) {
+    case "a" || "e" || "i" || "o" || "u":
+      letter = "A";
+      break;
+
+    case "b" || "c" || "d" || "f" || "g":
+      letter = "B";
+      break;
+
+    case "h" || "j" || "k" || "l" || "m":
+      letter = "C";
+      break;
+
+    default:
+      letter = "D";
+  }
+  return letter;
+};
---
+++ src/Day 2/Loops/script.js
@@ -0,0 +1,21 @@
+/**
+ * @author geekyharsh05
+ */
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+const vowelsAndConsonants = (s) => {
+  let vowels = ["a", "e", "i", "o", "u"];
+  for (let i = 0; i < s.length; i++) {
+    if (vowels.indexOf(s[i]) > -1) {
+      console.log(s[i]);
+    }
+  }
+  for (let j = 0; j < s.length; j++) {
+    if (vowels.indexOf(s[j]) < 0) {
+      console.log(s[j]);
+    }
+  }
+};
