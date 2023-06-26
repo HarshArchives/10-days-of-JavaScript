873f331df04ad1161ec0ac35eb85e9c4b2ac8567
geekyharsh05 <hp424805@gmail.com>
2023-06-26 21:32:56 +0530

    :sparkles: feat: add Day 8 Problems

---
+++ src/Day 8/Create a Button/button.js
@@ -0,0 +1,13 @@
+/**
+ * @author geekyharsh05
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("btn");
+
+  button.addEventListener("click", (e) => {
+    const count = Number(e.currentTarget.innerText) + 1;
+
+    e.currentTarget.innerText = count;
+  });
+});
